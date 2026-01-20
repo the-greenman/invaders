2001cd0bf20bddcd13fff5bff997a9e667b30213
Peter Brownell <greenman@the-organization.com>
2026-01-20 18:57:05 +0000

    1.0.10

--- src/version.ts
+++ src/version.ts
@@ -3,4 +3,4 @@
  * Auto-generated from package.json during build
  * Do not edit manually - changes will be overwritten
  */
-export const APP_VERSION = '1.0.9';
+export const APP_VERSION = '1.0.10';
